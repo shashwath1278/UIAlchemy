7e2883be06c6660eebcc42612996769ece83eae0
shash <1ms23is113@msrit.edu>
2025-03-24 22:22:37 +0530

    Update test-runner.js

--- test-runner.js
+++ test-runner.js
@@ -2,7 +2,7 @@ const { execSync } = require('child_process');
 const path = require('path');
 const fs = require('fs');
 
-// Clean previous test projects
+
 const testProjects = ['chakra-test', 'shade-test', 'mui-test', 'ant-test', 'boot-test', 'prime-test'];
 testProjects.forEach(proj => {
   if (fs.existsSync(proj)) {
@@ -10,15 +10,15 @@ testProjects.forEach(proj => {
   }
 });
 
-// Build the CLI
+
 console.log('🔨 Building CLI...');
 execSync('npm run build', { stdio: 'inherit' });
 
-// Install globally
+
 console.log('📦 Installing CLI globally...');
 execSync('npm install -g .', { stdio: 'inherit' });
 
-// Test each preset
+
 const presets = ['chakraflow', 'shadeflow', 'muitopia', 'antverse', 'bootflow', 'primeland'];
 
 presets.forEach(preset => {
